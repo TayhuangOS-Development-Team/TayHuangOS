8eb849843d4565625ef5d092466eb81d442c1765
theflysong <song_of_the_fly@163.com>
2022-06-04 14:04:08 +0800

    update

--- module/drivers/video/main.c
+++ module/drivers/video/main.c
@@ -1,3 +1,21 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * -------------------------------*-TayhuangOS-*-----------------------------------
+ *
+ *    Copyright (C) 2022, 2022 TayhuangOS Development Team - All Rights Reserved
+ *
+ * --------------------------------------------------------------------------------
+ *
+ * 作者: Flysong
+ *
+ * main
+ *
+ * 视频驱动主程序
+ *
+ */
+
+
+
 #include <debug/logging.h>
 
 void kmod_main(void) {
