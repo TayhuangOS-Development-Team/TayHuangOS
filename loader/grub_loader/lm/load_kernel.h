6133d2c86332a717bca4c7c81bf3c86643a964ca
theflysong <song_of_the_fly@163.com>
2022-07-16 17:22:36 +0800

    change license

--- loader/grub_loader/lm/load_kernel.h
+++ loader/grub_loader/lm/load_kernel.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-License-Identifier: GPL-3.0-only
+ * SPDX-License-Identifier: LGPL-2.1-only
  * -------------------------------*-TayhuangOS-*-----------------------------------
  * 
  *    Copyright (C) 2022, 2022 TayhuangOS Development Team - All Rights Reserved
