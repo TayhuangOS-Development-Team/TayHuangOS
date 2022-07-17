34e548fad91bd94199f8c1dd8947aea33b12e439
theflysong <song_of_the_fly@163.com>
2022-07-17 23:48:37 +0800

    update rpc in libkmod

--- libs/libkmod/syscall/rpc.c
+++ libs/libkmod/syscall/rpc.c
@@ -16,5 +16,87 @@
 
 
 
-#include <tayhuang/rpc.h>
+#include <syscall/rpc.h>
+#include <syscall/syscalls.h>
 
+#include <memory/kheap.h>
+
+#include <logging.h>
+
+#include <string.h>
+
+typedef struct  {
+    rpc_func func_no;
+    rpc_proccess_wrapper proccess;
+    rpc_size return_size;
+    rpc_size args_size;
+} proccess_info;
+
+//TODO: 使用红黑树
+typedef struct __proccess_node {
+    rpc_func func_no;
+    proccess_info *info;
+    struct __proccess_node *next;
+} proccess_node;
+
+PRIVATE proccess_node *head_node = NULL;
+
+PRIVATE proccess_info *list_find_proccess(rpc_func func_no) {
+    proccess_node *cur_node = head_node;
+    while (cur_node != NULL && cur_node->func_no != func_no) {
+        cur_node = cur_node->next;
+    }
+    if (cur_node == NULL)
+        return NULL;
+    return cur_node->info;
+}
+
+PRIVATE void add_proccess(rpc_func func_no, proccess_info *info) {
+    proccess_node *new_node = (proccess_node*)(kmalloc(sizeof(proccess_node)));
+
+    new_node->func_no = func_no;
+    new_node->info = info;
+    new_node->next = head_node;
+}
+
+PUBLIC void deal_rpc_request(void *msg, int caller) {
+    rpc_func func_no = ARG_READ(msg, rpc_func);
+
+    proccess_info *info = list_find_proccess(func_no);
+    if (info == NULL) {
+        return;
+    }
+
+    rpc_size args_size = ARG_READ(msg, rpc_size);
+    if (args_size != info->args_size) {
+        return;
+    }
+
+    rpc_size return_size = ARG_READ(msg, rpc_size);
+    if (return_size != info->return_size) {
+        return;
+    }
+
+    rpc_args_struct args = {.data = msg, .size = args_size};
+    rpc_args_struct result = info->proccess(caller, func_no, args);
+
+    if (info->return_size == result.size) {
+        send_msg(MSG_RPC_RESULT, result.data, result.size, caller);
+    }
+
+    kfree(result.data);
+}
+
+PUBLIC void rpc_register(rpc_func func, rpc_proccess_wrapper process, rpc_size return_size, rpc_size args_size) {
+    proccess_info *info = (proccess_info*)(kmalloc(sizeof(proccess_info)));
+    info->func_no = func;
+    info->proccess = process;
+    info->return_size = return_size;
+    info->args_size = args_size;
+    add_proccess(func, info);
+}
+
+PUBLIC rpc_args_struct rpc_call(int service, rpc_func func, rpc_args_struct args, rpc_size return_size, void *result) {
+    //TODO: 完成 rpc_call
+    return (rpc_args_struct){.data = NULL, .size = 0};
+}
