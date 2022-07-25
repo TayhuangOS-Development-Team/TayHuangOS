d3a3211e750e95145100f0b3dbd6486575e7a571
theflysong <song_of_the_fly@163.com>
2022-07-25 12:48:11 +0800

    init libfifo

---
+++ libs/libfifo/fifo.c
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-only
+ * -------------------------------*-TayhuangOS-*-----------------------------------
+ * 
+ *    Copyright (C) 2022, 2022 TayhuangOS Development Team - All Rights Reserved
+ * 
+ * --------------------------------------------------------------------------------
+ * 
+ * 作者: theflysong
+ * 
+ * fifo.c
+ * 
+ * First In First Out Buffer
+ * 
+ */
+
+
+
+#include <fifo.h>
+#include <debug/logging.h>
+
+PUBLIC void sayhello(void) {
+    linfo ("!!!");
+}
---
+++ libs/libfifo/fifo.h
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-only
+ * -------------------------------*-TayhuangOS-*-----------------------------------
+ * 
+ *    Copyright (C) 2022, 2022 TayhuangOS Development Team - All Rights Reserved
+ * 
+ * --------------------------------------------------------------------------------
+ * 
+ * 作者: theflysong
+ * 
+ * fifo.h
+ * 
+ * First In First Out Buffer
+ * 
+ */
+
+
+
+#pragma once
+
+#include <tayhuang/defs.h>
+
+PUBLIC void sayhello(void);
